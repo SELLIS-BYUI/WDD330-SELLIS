cfafcd58d057058873ec751a2ce223077251669c
SELLIS-BYUI <63525626+SELLIS-BYUI@users.noreply.github.com>
2020-05-23 22:16:06 -0600

    updated. Week 5 info

---
+++ week6/toDo.js
@@ -0,0 +1,6 @@
+let firstName = prompt("what is your first name");
+let lastName = prompt("what is your last name");
+let age = prompt("what is your age");
+
+console.log("your full name is " + firstName + " " + lastName);
+console.log("Your are " + age + " years old." );
