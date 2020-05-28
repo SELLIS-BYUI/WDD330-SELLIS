2f233a2f2b96db6a0025fdb0963f98605d72b4f8
SELLIS-BYUI <63525626+SELLIS-BYUI@users.noreply.github.com>
2020-05-27 19:58:55 -0600

    updated menu css

--- week6/toDo.js
+++ week6/toDo.js
@@ -1,6 +1,6 @@
-let firstName = prompt("what is your first name");
-let lastName = prompt("what is your last name");
-let age = prompt("what is your age");
+class ToDo {
+    constructor(userInput) {
+        this.userInput = userInput;
+    }
+}
 
-console.log("your full name is " + firstName + " " + lastName);
-console.log("Your are " + age + " years old." );
