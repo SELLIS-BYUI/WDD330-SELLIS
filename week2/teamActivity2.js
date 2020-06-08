83dd536593d0593f648315ef8487a19adb22d2cc
SELLIS-BYUI <63525626+SELLIS-BYUI@users.noreply.github.com>
2020-06-08 08:19:22 -0600

    week8

--- week2/teamActivity2.js
+++ week2/teamActivity2.js
@@ -1,19 +1,29 @@
-function names() {
+var clickBtn = document.getElementById("click");
+clickBtn.addEventListener("click", getNum1);
+clickBtn.addEventListener("click", getNumTwoAdd);
 
-    var num= document.getElementById("input").value;
-    var result = numberSum(num);
-     console.log(result);
-     document.getElementById("name").innerHTML=result;
- 
- }
- 
- function numberSum(num) {
-     console.log(num);
-     var a;
-     var total=0;
-     for(i=0; i<=num; i++){
-         total += i;
-     }
-     return total;
- 
- } 
+
+function getNum1() {
+    var num1 = document.getElementById('input1').value;
+    document.getElementById('outPut').innerHtml = num1;
+}
+function getNumTwoAdd() {
+    var num1 = document.getElementById('input1').value;
+    var num2 = document.getElementById('input2').value;
+    
+    var numCheck1 = parseInt(num1);
+    var numCheck2 = parseInt(num2);
+
+    var total = 0;
+
+    if(numCheck1 !== NaN && numCheck2 !== NaN){
+        for (var i = 0; i <= numCheck2; i++) {
+            total += i;
+            document.getElementById("outPut2").innerHTML = total;
+        }
+    };
+    var grandTotal = numCheck1 + numCheck2;
+    document.getElementById('total').innerHTML = grandTotal;
+       
+}
+
