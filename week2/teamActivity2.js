f337c153b77d0d49a9a5a289ff851cbad44b7766
SELLIS-BYUI <63525626+SELLIS-BYUI@users.noreply.github.com>
2020-06-09 06:32:30 -0600

    more changes

--- week2/teamActivity2.js
+++ week2/teamActivity2.js
@@ -29,7 +29,31 @@ function getTotal() {
     var getNumTwo = document.getElementById('inputTwo').value;
     var numOneConvert = parseInt(getNumOne);
     var numTwoConvert = parseInt(getNumTwo);
-    var grandTotal = numOneConvert + numTwoConvert;
     document.getElementById('sumTotal').innerHTML = 
-    `Grand Total of Number one and two: ${grandTotal}`;
+    `Grand Total of Number one and two: ${numOneConvert + numTwoConvert}`;
 };
+
+/**************************************************
+ * This is our team effort with JavaScript. But have made changes to improve
+ * skill and understand of advance JavaScript.
+ * 
+ * function names() {
+
+   var num= document.getElementById("input").value;
+   var result = numberSum(num);
+    console.log(result);
+    document.getElementById("name").innerHTML=result;
+
+}
+
+function numberSum(num) {
+    console.log(num);
+    var a;
+    var total=0;
+    for(i=0; i<=num; i++){
+        total += i;
+    }
+    return total;
+
+} 
+ */
