0b7310498b0fc47bfbac1dd5874713785de3d15f
SELLIS-BYUI <63525626+SELLIS-BYUI@users.noreply.github.com>
2020-06-12 17:45:17 -0600

    adding new material

--- week8/teamActivity8.js
+++ week8/teamActivity8.js
@@ -1,7 +1,48 @@
-fetch('https://pokeapi.co/api/v2/pokemon/')
-    .then(response => {
-        return response.json()})
-    .then(data => {
-        console.log(data)})
+let nextBTN = document.querySelector('#nextBTN');
+let prevBTN = document.querySelector('#prevBTN')
+nextBTN.addEventListener('click', navigation);
+prevBTN.addEventListener('click', navigation);
+let next, previous;
 
-    
+
+fetch('https://pokeapi.co/api/v2/pokemon/')
+  .then(response => response.json())
+  .then(pokeDex => {
+        let createListItems =  document.querySelector("#newList");
+        createListItems.innerHTML = '';
+        pokeDex.results.forEach(result => {
+           createListItems.innerHTML += `<li class="pokeAbilities">${result.name}</li>`; 
+           
+        });
+        next = pokeDex.next;
+        previous = pokeDex.previous;
+  });
+
+  function navigation() {
+    if(next){
+        fetch(next)
+            .then(respond => respond.json())
+            .then(pokeDex => {
+                let createListItems =  document.querySelector("#newList");
+                createListItems.innerHTML = '';
+                pokeDex.results.forEach(result => {
+                   createListItems.innerHTML += `<li class="pokeAbilities">${result.name}</li>`; 
+                });
+                next = pokeDex.next;
+                previous = pokeDex.previous;
+            })
+        };
+    if(previous){
+        fetch(previous)
+            .then(respond => respond.json())
+            .then(pokeDex => {
+                let createListItems =  document.querySelector("#newList");
+                createListItems.innerHTML = '';
+                pokeDex.results.forEach(result => {
+                   createListItems.innerHTML += `<li class="pokeAbilities">${result.name}</li>`; 
+                });
+                next = pokeDex.next;
+                previous = pokeDex.previous;
+            })
+        };
+    }
